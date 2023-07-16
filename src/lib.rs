a241965c0fd8391506e5c6d1ca11b29d468da965
asciiboi <56585404+asciiboi@users.noreply.github.com>
2023-07-16 20:50:02 +0200

    Added comments.

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,12 @@ pub struct Bhtmp {
 
 impl Bhtmp {
     pub fn new(bytes: Vec<u8>) -> Self {
+        // Get the header
         let headers = bytes.get(0..16).unwrap();
+        // Get everything after the header.
         let data = bytes.get(16..).unwrap();
+        
+        // Read segments of 8 bytes into records.
         let mut records: Vec<DataRecord> = vec![];
         for i in data.chunks(8) {
             records.push(DataRecord {
@@ -28,7 +32,7 @@ impl Bhtmp {
 
     pub fn to_image(self: &Self, bitidx: usize) -> RgbImage {
         let mut image: RgbImage = ImageBuffer::new(100, 100);
-
+        
         for (i, rec) in self.records.iter().enumerate() {
             image.put_pixel(
                 (i % 100) as u32,
